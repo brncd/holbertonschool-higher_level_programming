63b770d3cf3639aea2a3252679672808930bf199
brncd <brunocarnales@gmail.com>
2023-01-20 05:09:04 -0800

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import add_0
-a, b = 1, 2
-print("{} + {} =".format(a, b), add_0.add(a, b))
+if __name__ == "__main__":
+    import add_0
+    a, b = 1, 2
+    print("{} + {} =".format(a, b), add_0.add(a, b))
