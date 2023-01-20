55687e52f408eadf56fbe312491c38f58777222c
brncd <brunocarnales@gmail.com>
2023-01-20 06:44:59 -0800

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments".format(len(sys.argv) - 1))
-    if len(sys.argv) > 0:
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+    if (len(sys.argv)) == 1:
+        dot = "."
+    else:
+        dot = ":"
+    if (len(sys.argv)) == 2:
+        print("{} argument{}".format(len(sys.argv) - 1, dot))
+    else:
+        print("{} arguments{}".format(len(sys.argv) - 1, dot))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
