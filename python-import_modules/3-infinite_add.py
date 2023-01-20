3a1b1259005c4cbff4396dcf437d38d81da3d084
brncd <brunocarnales@gmail.com>
2023-01-20 12:17:19 -0800

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
     import sys
     if sys.argv[1:]:
         a = int(sys.argv[1])
         b = int(sys.argv[2])
         for i in range(3, len(sys.argv)):
             b += int(sys.argv[i])
-        print("{}".format(add(a, b)))
+        print("{}".format(a + b))
     else:
         print("0")
