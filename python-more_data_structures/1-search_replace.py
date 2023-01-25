3fa3e69b35b1902e0192d4a34281bd329bd4dd97
brncd <brunocarnales@gmail.com>
2023-01-25 07:01:04 -0800

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    a = lambda x, y: x if x != search else y
-    return list(map(a, my_list, [replace] * len(my_list)))
-
+    return list(map(lambda x, y: x if x != search else y,
+                    my_list, [replace] * len(my_list)))
