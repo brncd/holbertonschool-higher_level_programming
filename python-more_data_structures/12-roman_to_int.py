a75858aff6a333d6a6cc5924990b7641531fa2d7
brncd <brunocarnales@gmail.com>
2023-01-27 04:53:21 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romanum_dict = {'I': "1", 'V': "5", 'X': "10", 'L': "50", 'C': "100", 'D': "500", 'M': "1000"}
-    numers = {i: int(j) for i, j in romanum_dict.items()}
-    return sum(numers[i] for i in roman_string)
+    romanum_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in range(len(roman_string) - 1):
+        if romanum_dict[roman_string[i]] < romanum_dict[roman_string[i + 1]]:
+            romanum_dict[roman_string[i]] = -romanum_dict[roman_string[i]]
+    return sum(romanum_dict[i] for i in roman_string)
