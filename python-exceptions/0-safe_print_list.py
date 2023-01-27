8926f1c5b3f0f180bd36cce2dadaeac47f459c22
brncd <brunocarnales@gmail.com>
2023-01-27 11:51:39 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        if x > len(my_list):
-            x = len(my_list)
-        for i in my_list:
-            if i <= x:
-                print("{}".format(i), end="")
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
         print()
         return x
-    except IndexError:
+    except:
         print()
         return i
