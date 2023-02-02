efd0e5587becf3b7decf530be95ce390128b1b5e
brncd <brunocarnales@gmail.com>
2023-02-02 05:29:07 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+class Square:
+
+    """Square class."""
+
+    def __init__(self, size=0):
+        """Initialize square."""
+        self.__size = size
