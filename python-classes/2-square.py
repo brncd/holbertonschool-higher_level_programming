62436f2e80373f81f0ee2f1e3952b3e00be12427
brncd <brunocarnales@gmail.com>
2023-02-02 07:55:53 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Holberton School."""
+
+
+class Square:
+
+    """Square class."""
+
+    def __init__(self, size=0):
+        """Initialize square."""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
