6f04773cdb799818943b173ca8d4f16a1fe7eeec
brncd <brunocarnales@gmail.com>
2023-02-03 14:25:26 -0800

    9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__height = value
 
     @property
@@ -87,4 +87,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle instance with width == height == size."""
-        return Rectangle(width=size, height=size)
+        return cls(size, size)
