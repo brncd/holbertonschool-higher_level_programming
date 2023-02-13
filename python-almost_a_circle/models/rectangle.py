401ce43bc708148428cad5afac8bf59fd7ce9a43
brncd <brunocarnales@gmail.com>
2023-02-13 08:36:17 -0800

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,18 +2,18 @@
 """First Rectangle."""
 from models.base import Base
 
-
 class Rectangle(Base):
-    """Rectangle class"""
-    _width = 0
-    _height = 0
-    _x = 0
-    _y = 0
+    """Class Rectangle."""
+    __width = None
+    __height = None
+    __x = None
+    __y = None
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor."""
+        """Initialize Rectangle."""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
+    
