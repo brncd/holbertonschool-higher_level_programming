660952c3001ca24640019d3bcbf0c608aaf7a65c
brncd <brunocarnales@gmail.com>
2023-06-04 15:57:45 -0700

    checker

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-#from sys import argv
-#if __name__ == "__main__":
-#    args = argv[1:]
-#    print(len(args), "argument" if len(args) <= 1 else "arguments", end="")
-#    print("." if len(args) == 0 else ":")
-#    for idx, arg in enumerate(args, start=1):
-#        print(idx, ":", arg)
 if __name__ == "__main__":
     from sys import argv
     num = len(argv) - 1
