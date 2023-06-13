f268f674c8a111540b36cbbf23741833cc7851ab
brncd <brunocarnales@gmail.com>
2023-06-13 06:36:34 -0700

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python - More Classes and Objects"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self):
+        """Initialize rectangle"""
+        Rectangle.rectangle = {}
