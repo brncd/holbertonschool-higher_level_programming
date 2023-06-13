6e5a4dd24181ccb8c6c76914b099f12073bc1385
brncd <brunocarnales@gmail.com>
2023-06-13 06:00:28 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""0. My first square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Constructor"""
+        Square.square = {}
+        self.__size__ = size
