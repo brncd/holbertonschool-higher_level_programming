e587c7ccabb755c48a389056021b892fe25b2e09
brncd <brunocarnales@gmail.com>
2023-06-23 09:33:18 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty clas"""
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
