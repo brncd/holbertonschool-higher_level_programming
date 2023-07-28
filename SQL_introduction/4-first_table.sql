905edceafa0ef0df33d0105b8c655dcc878ac2e0
brncd <brunocarnales@gmail.com>
2023-07-28 12:22:19 -0300

    4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE if NOT EXISTS 'first_table' (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
